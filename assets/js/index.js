7461810ec200bbfba6481557971a28ac77894a02
gustavoOchoa <gustavoeochoa@gmail.com>
2023-06-30 16:06:57 -0300

    nuevo armado para educational

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,28 @@
 window.addEventListener('load', function() {
     resizeHome();
+    observer.observe(document.querySelector('.profile'));
+
 });
 
 window.visualViewport.addEventListener("resize", () => {
     resizeHome();
+});
+
+window.addEventListener('scroll', (event) => {
+
+});
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            if(entry.target.classList.contains('profile')){
+                entry.target.classList.add('animate__animated');
+                entry.target.classList.add('animate__flipInX');
+                entry.target.classList.add('animate__delay-1s');
+            }
+        }
+    });
 });
 
 function resizeHome() {
@@ -11,3 +30,4 @@ function resizeHome() {
     let sectionHome = this.document.getElementById('home');
     sectionHome.style.height = height+'px';
 }
+
