5e1057737acd3769c28d613e25d70b3a46965043
gustavoOchoa <gustavoeochoa@gmail.com>
2023-07-05 08:37:34 -0300

    hasta las barras

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,72 @@
 window.addEventListener('load', function() {
     resizeHome();
     observer.observe(document.querySelector('.profile'));
+    observer.observe(document.querySelector('.selfie'));
+    let cards = document.querySelectorAll('.edu-card');
+    cards.forEach(card => {
+        observer.observe(card);
+    });
 
+    let bars = document.querySelectorAll('.skill-bar');
+    bars.forEach(bar => {
+        barFill.observe(bar);
+    });
 });
 
 window.visualViewport.addEventListener("resize", () => {
     resizeHome();
 });
 
-window.addEventListener('scroll', (event) => {
-
-});
-
 const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            if(entry.target.classList.contains('profile')){
-                entry.target.classList.add('animate__animated');
-                entry.target.classList.add('animate__flipInX');
-                entry.target.classList.add('animate__delay-1s');
+            if(entry.target.classList.contains('profile')){ /*** profile info */
+                addDeleteClass('add', ['animate__animated', 'animate__flipInX', 'animate__slow'], entry);
+            }
+            if(entry.target.classList.contains('selfie')){ /*** long text */
+                addDeleteClass('add', ['animate__animated', 'animate__fadeIn', 'animate__slow', 'animate__delay-1s'], entry);
+            }
+            if(entry.target.classList.contains('edu-card')){ /*** card */
+                addDeleteClass('add', ['animate__animated', 'animate__flipInY', 'animate__slow', 'animate__delay-1s'], entry);
+            }
+        }
+        else{
+            if(entry.target.classList.contains('profile')){ /*** profile info */
+                addDeleteClass('del', ['animate__animated', 'animate__flipInX', 'animate__slow'], entry);
+            }
+            if(entry.target.classList.contains('selfie')){ /*** long text */
+                addDeleteClass('del', ['animate__animated', 'animate__fadeIn', 'animate__slow', 'animate__delay-1s'], entry);
+            }
+            if(entry.target.classList.contains('edu-card')){ /*** card */
+                addDeleteClass('del', ['animate__animated', 'animate__flipInY', 'animate__slow', 'animate__delay-1s'], entry);
             }
         }
     });
-});
+}, { threshold: [0] });
+
+const barFill = new IntersectionObserver(bars => {
+    bars.forEach(bar => {
+        let width = bar.target.getAttribute('aria-describedby');
+        let children = [...bar.target.children];
+
+        if (bar.isIntersecting){
+            children.forEach(item => {
+                if(item.target.classList.contains('colored')){
+                    item.target.classList.add('i'+width);
+                }
+            });
+        }
+        else{
+            children.forEach(item => {
+                if(item.target.classList.contains('colored')){
+                    item.target.classList.remove('i'+width);
+                }
+            });
+        }
+        
+        
+    });
+}, { threshold: [0] });
 
 function resizeHome() {
     let height = this.window.innerHeight;
@@ -31,3 +74,19 @@ function resizeHome() {
     sectionHome.style.height = height+'px';
 }
 
+function addDeleteClass(type, classes, entity){
+    if(type === 'add'){
+        entity.target.classList.remove('noShow');
+        entity.target.classList.add('show');
+        classes.forEach(element => {
+            entity.target.classList.add(element);
+        });
+    }
+    else{
+        entity.target.classList.add('noShow');
+        entity.target.classList.remove('show');
+        classes.forEach(element => {
+            entity.target.classList.remove(element);
+        });
+    }
+}
